3c107abb66b310f38291c6f4d1590aaf54e7a3fa
Dhren <roglobar10@gmail.com>
2025-08-26 14:33:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "dotenv/config";
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
